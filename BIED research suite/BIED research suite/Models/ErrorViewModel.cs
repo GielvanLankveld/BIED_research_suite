d7e54949df9e607a02a6bfcd3ae4b6ecb44f33bf
MrGiel <gielvanlankveld@gmail.com>
2020-06-26 16:38:30 +0200

    Initial commit

---
+++ BIED research suite/BIED research suite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BIED_research_suite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
