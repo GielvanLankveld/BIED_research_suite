1e608176131f0d3d9ab4a5368fedb2dea80e3509
MrGiel <gielvanlankveld@gmail.com>
2020-07-02 13:12:28 +0200

    Correcte koppeling tussen alle controllers, views en page layouts

---
+++ BIED research suite/BIED research suite/Models/ResearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIED_research_suite.Models
+{
+    public class ResearchViewModel
+    {
+    }
+}
